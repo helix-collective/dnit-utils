70c2c511a224711710ab9e214f5ad945dff3d861
Paul Thompson <paul@helixta.com.au>
2020-09-02 11:57:27 +1000

    Add export of docker functions

--- mod.ts
+++ mod.ts
@@ -1,5 +1,9 @@
 // Exporting for use by other projects
 
+export * from "./docker/image.ts";
+export * from "./docker/login.ts";
+export * from "./docker/run.ts";
+
 export * from "./git.ts";
 export * from "./gitTasks.ts";
 export * from "./io.ts";
