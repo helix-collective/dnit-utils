6d59eb8ab5ba70edbe228a5618357bd256d8f1b3
Paul Thompson <paul@helixta.com.au>
2021-06-30 00:01:15 +1000

    Update dependencies (#5)
    Fix for error:
    error: TS2339 [ERROR]: Property 'operator' does not exist on type 'never'.
        if (high!.operator === comp || high!.operator === ecomp) {
                  ~~~~~~~~
        at https://deno.land/x/semver@v1.0.0/mod.ts:1744:15

--- deps.ts
+++ deps.ts
@@ -6,7 +6,7 @@ import * as hash from "https://deno.land/std@0.97.0/hash/mod.ts";
 import * as io from "https://deno.land/std@0.97.0/io/mod.ts";
 import * as log from "https://deno.land/std@0.97.0/log/mod.ts";
 import * as path from "https://deno.land/std@0.97.0/path/mod.ts";
-import * as semver from "https://deno.land/x/semver@v1.0.0/mod.ts";
+import * as semver from "https://deno.land/x/semver@v1.4.0/mod.ts";
 import * as uuid from "https://deno.land/std@0.97.0/uuid/mod.ts";
 
 export { flags, fs, hash, io, log, path, semver, uuid };
